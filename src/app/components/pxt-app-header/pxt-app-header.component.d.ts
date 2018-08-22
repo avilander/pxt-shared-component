b8babfae267e5e2c647764a17e5b31216fb66223
PXTUDI\avilandersc <avilandersc@peixoto.com.br>
2018-08-22 11:51:58 -0300

    remove

--- src/app/components/pxt-app-header/pxt-app-header.component.d.ts
+++
@@ -1,5 +0,0 @@
-import { OnInit } from '@angular/core';
-export declare class PxtAppHeaderComponent implements OnInit {
-    constructor();
-    ngOnInit(): void;
-}
--- src/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { PxtAppHeaderModule } from './app/components/pxt-app-header/pxt-app-header.module';
-export { PxtAppHeaderComponent } from './app/components/pxt-app-header/pxt-app-header.component';
