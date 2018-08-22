5b824907531084086e8ca3ab0b90f609afb0bda0
PXTUDI\avilandersc <avilandersc@peixoto.com.br>
2018-08-22 11:33:42 -0300

    first commit

---
+++ src/index.d.ts
@@ -0,0 +1,2 @@
+export { PxtAppHeaderModule } from './app/components/pxt-app-header/pxt-app-header.module';
+export { PxtAppHeaderComponent } from './app/components/pxt-app-header/pxt-app-header.component';
