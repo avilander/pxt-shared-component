391ada25fccbacff7bcf3735d8790bf4c1deb571
PXTUDI\avilandersc <avilandersc@peixoto.com.br>
2018-08-22 11:48:15 -0300

    retirada

--- src/app/components/modules/angular-material.module.ngfactory.d.ts
+++
@@ -1,3 +0,0 @@
-import * as i0 from '@angular/core';
-import * as i1 from './angular-material.module';
-export declare const AngularMaterialModuleNgFactory: i0.NgModuleFactory<i1.AngularMaterialModule>;
--- src/index.js
+++
@@ -1,3 +0,0 @@
-export { PxtAppHeaderModule } from './app/components/pxt-app-header/pxt-app-header.module';
-export { PxtAppHeaderComponent } from './app/components/pxt-app-header/pxt-app-header.component';
-//# sourceMappingURL=index.js.map
